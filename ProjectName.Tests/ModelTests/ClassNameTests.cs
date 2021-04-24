3dbdb64a6327eda52cb587a25d68b4e009fe0eea
Cat Denton <76975697+Cat-Denton@users.noreply.github.com>
2021-04-24 10:59:56 -0700

    Initial commit

---
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    
+  }
+}
