3f98fe596e2dd16bc64d4a842f8eeb076486025f
Cat-Denton <willwdenton@gmail.com>
2021-04-24 18:45:08 -0700

    pass GetPastryDiscount_ReturnDiscountThreePastries_1 test

--- CatsBakery/Models/Pastry.cs
+++ CatsBakery/Models/Pastry.cs
@@ -16,6 +16,12 @@ namespace CatsBakery.Models
     {
       int discountCount = 0;
       int discountAmount = 1;
+
+      if(PastryOrderSize % 3 == 0)
+      {
+        discountCount++;
+      }
+
       int pastryDiscount = discountAmount*discountCount;
       return pastryDiscount; 
     }
